e2997875efbfbd7fdeed63e70223d18086565a5f
Akash Kadam <101860317+akashkadam01@users.noreply.github.com>
2024-11-06 11:37:22 +0530

    added mkcert ssl to localhost

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using System.Reflection.Metadata;
+using System;
 using System.Collections.Immutable;
 using API.Data;
 using Microsoft.EntityFrameworkCore;
@@ -10,12 +12,13 @@ builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("Http://localhost:4200","Https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
